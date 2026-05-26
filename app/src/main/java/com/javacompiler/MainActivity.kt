c23aa4535ca7b3b9817cd599c05d270152274f14
Kingjha13 <avnishjha277@gmail.com>
2026-05-26 22:46:02 +0530

    Initial commit

---
+++ app/src/main/java/com/javacompiler/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.javacompiler
+
+import android.graphics.BlurMaskFilter
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.javacompiler.ui.theme.JavaCompilerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JavaCompiler()
+        }
+    }
+}
+
+@Composable
+fun JavaCompiler() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Text("NovaLang")
+            Text(
+                text = "Build • Parse • Execute",
+                color = Color.Blue
+            )
+        }
+    }
+}
